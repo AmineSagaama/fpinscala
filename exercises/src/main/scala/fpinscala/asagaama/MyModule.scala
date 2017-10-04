51ac431e9524ea5f3c074637b89ca2bd059bcb75
Amine Sagaama <amine.sagaama@gmail.com>
2017-10-04 07:46:15 +0200

    implement a recursive function to get the nth Fibonacci number

--- exercises/src/main/scala/fpinscala/asagaama/MyModule.scala
+++ exercises/src/main/scala/fpinscala/asagaama/MyModule.scala
@@ -15,4 +15,26 @@ object MyModule {
     if (n < 0) -n
     else n
 
+  // A definition of factorial, using a local, tail recursive function
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+
+    go(n, 1)
+  }
+
+  // 0 and 1 are the first two numbers in the sequence,
+  // so we start the accumulators with those.
+  // At every iteration, we add the two numbers to get the next one.
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def loop(n: Int, prev: Int, cur: Int): Int =
+      if (n == 0) prev
+      else loop(n - 1, cur, prev + cur)
+
+    loop(n, 0, 1)
+  }
+
 }
